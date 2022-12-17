43ff75bd5fb9c766495fea581da345db1cb9144d
VICTOIREMICHELDLM <victoiremicheldlm@gmail.com>
2022-12-17 16:26:14 +0100

    added the post route

--- app.js
+++ app.js
@@ -18,15 +18,15 @@ const movieHandlers = require("./movieHandlers");
 
 app.get("/api/movies", movieHandlers.getMovies);
 app.get("/api/movies/:id", movieHandlers.getMovieById);
+app.post("/api/movies", movieHandlers.postMovie);
+app.put("/api/movies/:id", movieHandlers.updateMovie);
 
 const usersHandlers = require("./usersHandlers");
 
 app.get("/api/users", usersHandlers.getUsers);
 app.get("/api/users/:id", usersHandlers.getUsersById);
-
-app.post("/api/movies", movieHandlers.postMovie);
 app.post("/api/users", usersHandlers.postUser);
-
+app.put("/api/users/:id", usersHandlers.updateUser);
 
 app.listen(port, (err) => {
   if (err) {
--- usersHandlers.js
+++ usersHandlers.js
@@ -44,16 +44,38 @@ const getUsers = (req, res) => {
       }
   })
         .catch((err) => {
-          consle.erorr(err);
+          console.error(err);
           res.Status(500).send("Error retrieving data from database");
         })
       };
 
 
+      const updateUser = (req, res) => {
+        const id = parseInt(req.params.id);
+        const { firstname, lastname, email, city, language } = req.body;
+      
+        database
+          .query(
+            "update users set firstname = ?, lastname = ?, email = ?, city = ?, language = ? where id = ?",
+            [firstname, lastname, email, city, language, id]
+          )
+          .then(([result]) => {
+            if (result.affectedRows === 0) {
+              res.status(404).send("Not Found");
+            } else {
+              res.sendStatus(204);
+            }
+          })
+          .catch((err) => {
+            console.error(err);
+            res.status(500).send("Error editing the users");
+          });
+      };
 
 module.exports = {
     getUsers,
     getUsersById,
     postUser,
+    updateUser,
   };
   
