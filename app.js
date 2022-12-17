ed41d9a8ad84976d2074ed85f42430fa7f742043
VICTOIREMICHELDLM <victoiremicheldlm@gmail.com>
2022-12-17 15:29:41 +0100

    created post route

--- app.js
+++ app.js
@@ -1,8 +1,12 @@
+require("dotenv").config();
+
 const express = require("express");
 
 const app = express();
 
-const port = 5000;
+app.use(express.json());
+
+const port = process.env.APP_PORT ?? 5001;
 
 const welcome = (req, res) => {
   res.send("Welcome to my favourite movie list");
@@ -15,6 +19,15 @@ const movieHandlers = require("./movieHandlers");
 app.get("/api/movies", movieHandlers.getMovies);
 app.get("/api/movies/:id", movieHandlers.getMovieById);
 
+const usersHandlers = require("./usersHandlers");
+
+app.get("/api/users", usersHandlers.getUsers);
+app.get("/api/users/:id", usersHandlers.getUsersById);
+
+app.post("/api/movies", movieHandlers.postMovie);
+app.post("/api/users", usersHandlers.postUser);
+
+
 app.listen(port, (err) => {
   if (err) {
     console.error("Something bad happened");
@@ -22,3 +35,4 @@ app.listen(port, (err) => {
     console.log(`Server is listening on ${port}`);
   }
 });
+
---
+++ usersHandlers.js
@@ -0,0 +1,59 @@
+const database = require("./database");
+
+
+  const postUser = (req, res) => {
+    const { firstname, lastname, email, city, language } = req.body;
+  
+    database
+      .query(
+        "INSERT INTO users(firstname, lastname, email, city, language) VALUES (?, ?, ?, ?, ?)",
+        [firstname, lastname, email, city, language]
+      )
+      .then(([result]) => {
+        res.location(`/api/users/${result.insertId}`).sendStatus(201);
+      })
+      .catch((err) => {
+        console.error(err);
+        res.status(500).send("Error saving the user");
+      });
+  };
+
+const getUsers = (req, res) => {
+    database
+    .query("select * from users")
+    .then(([users]) => {
+      res.json(users);
+    })
+    .catch((err) => {
+      console.error(err);
+      res.status(500).send("Error retrieving data from database ");
+    });
+  };
+  
+  
+  const getUsersById = (req, res) => {
+    const id = parseInt(req.params.id);
+  
+    database
+    .query("select * from users where id = ?", [id])
+    .then (([users]) => {
+      if (users[0] =! null) {
+        res.json(users[0]);
+      } else {
+        res.status(404).send("Not Found");
+      }
+  })
+        .catch((err) => {
+          consle.erorr(err);
+          res.Status(500).send("Error retrieving data from database");
+        })
+      };
+
+
+
+module.exports = {
+    getUsers,
+    getUsersById,
+    postUser,
+  };
+  
